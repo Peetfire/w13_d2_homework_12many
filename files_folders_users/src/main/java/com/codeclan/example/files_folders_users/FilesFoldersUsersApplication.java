157321a083d97ebabc8a0180ddc4f67ae300ac1c
petermordaunt <peetfire@hotmail.com>
2022-02-23 06:23:27 +0000

    Setup - created project files

---
+++ files_folders_users/src/main/java/com/codeclan/example/files_folders_users/FilesFoldersUsersApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.files_folders_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesFoldersUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesFoldersUsersApplication.class, args);
+	}
+
+}
---
+++ files_folders_users/src/test/java/com/codeclan/example/files_folders_users/FilesFoldersUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.files_folders_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesFoldersUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
